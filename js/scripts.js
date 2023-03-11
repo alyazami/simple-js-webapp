dc65f5832a47ab9af65cf73c466e0f72b3855051
alyazami <104652840+alyazami@users.noreply.github.com>
2023-03-11 11:39:25 -0600

    created the alert and removed practice codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,1 @@
-let myName = 'Aziz';
-document.write(myName);
-
-
-
-let size = 100;
-let doubleSize = size * 2;
-let miniSize = (doubleSize / 2) - (size / 2);
-document.write(miniSize);
+alert('Hello world');
