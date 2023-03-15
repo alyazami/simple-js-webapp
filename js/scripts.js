53e9acca319cb21ccb4820ec3cd94984895a1b84
alyazami <104652840+alyazami@users.noreply.github.com>
2023-03-15 10:38:03 -0500

    pokemon name and height
    printing pokemon name and height & hightlight the one with big and small hieght value

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,33 @@
-let pokemonlist = [
+let pokemonList = [
     {
-        name: 'barbasure',
+        name: 'Barbasure',
         height: 7,
         type: ['grass', 'poison']
     },
     {
-        name: 'pikatchu',
+        name: 'Pikatchu',
         height: 0.4,
         type: ['static', 'lightningrod']
 
     },
     {
-        name: 'charmeleon',
+        name: 'Charmeleon',
         height: 1.1,
         type: ['blaze', 'solar-power']
     }
 ]
+
+// printing pokemon name and height & hightlight the one with big and small hieght value 
+for (let i=0; i < pokemonList.length; i++){
+    let namePrinted = false;  // flag variable to keep track of whether name is already printed
+    if (pokemonList[i].height > 6) {
+        document.write(pokemonList[i].name + ' ( '+ pokemonList[i].height +') ' + ' : Wow, that\'s big\!' + '<br>');
+        namePrinted = true;
+    } else if ( pokemonList[i].height< 0.5 ) {
+        document.write(pokemonList[i].name + ' ( '+ pokemonList[i].height + ') ' + ' : What a tiny pokemon\!' + '<br>');
+        namePrinted = true;
+    }
+    if (!namePrinted) {  // print the name and height only if it's not already printed above
+        document.write(pokemonList[i].name + ' ('+ pokemonList[i].height +') ' + '<br>');
+    }
+}
