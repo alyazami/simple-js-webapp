6ad5175069531a91068eca9829c1fa7a5c0bffaa
alyazami <104652840+alyazami@users.noreply.github.com>
2023-03-22 10:10:32 -0500

    apply IFFY and comment my code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-let pokemonList = [
+// Create an Immediately Invoked Function Expression (IIFE) to wrap the pokemonList array
+let pokemonRepository = (function () {
+  let pokemonList = [
     {
         name: 'Barbasure',
         height: 7,
@@ -15,20 +17,43 @@ let pokemonList = [
         height: 1.1,
         type: ['blaze', 'solar-power']
     }
-]
+  ];
 
-// printing pokemon name and height & hightlight the one with big and small hieght value 
-pokemonList.forEach(function(pokemon) {
+  // Define a function to return the entire pokemonList array
+  function getAll() {
+    return pokemonList;
+  }
+
+  // Define a function to add a new item to the pokemonList array
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  // Return an object with the two functions as properties
+  return {
+    getAll: getAll,
+    add: add
+  };
+})();
+
+// Use the getAll function to retrieve the pokemonList array and iterate over it
+pokemonRepository.getAll().forEach(function(pokemon) {
   let namePrinted = false;  // flag variable to keep track of whether name is already printed
   if (pokemon.height > 6) {
     document.write('<span class="pokemon-highlight">' + pokemon.name + ' ('+ pokemon.height +')</span>');
     document.write('<span class="pokemon-message"> : Wow, that\'s big\!</span>' + '<br>');
   } else if (pokemon.height < 0.5) {
-  document.write('<span class="pokemon-highlight">' + pokemon.name + ' ('+ pokemon.height +')</span>');
-  document.write('<span class="pokemon-message"> : What a tiny pokemon\!</span>' + '<br>');
-  namePrinted = true;
-} else {
-  document.write('<span class="pokemon">' + pokemon.name + ' ('+ pokemon.height +')</span>' + '<br>');
-}
-  
+    document.write('<span class="pokemon-highlight">' + pokemon.name + ' ('+ pokemon.height +')</span>');
+    document.write('<span class="pokemon-message"> : What a tiny pokemon\!</span>' + '<br>');
+    namePrinted = true;
+  } else {
+    document.write('<span class="pokemon">' + pokemon.name + ' ('+ pokemon.height +')</span>' + '<br>');
+  }
+});
+
+// The pokemonList array cannot be accessed directly from outside the IIFE, but can be modified using the add function
+pokemonRepository.add({
+  name: 'Bulbasaur',
+  height: 0.7,
+  type: ['grass', 'poison']
 });
